fdb9fd2019fd3f990c7bb9c60b940e4b2d056e47
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-11-01 10:34:21 +0700

    Initial commit

---
+++ Lab8_demo.Test/ArrayUtilsTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_demo.Test
+{
+    [TestClass]
+    public class ArrayUtilsTest
+    {
+        [TestMethod]
+        public void FindMax_ValidArray_ReturnsMax()
+        {
+            int[] array = { 3, 7, 1, 9, 4 };
+            int expectedMax = 9;
+
+            int actualMax = ArrayUtils.FindMax(array);
+
+            Assert.AreEqual(expectedMax, actualMax);
+        }
+
+        [TestMethod]
+        public void FindMin_ValidArray_ReturnsMin()
+        {
+            int[] array = { 3, 7, 1, 9, 4 };
+            int expectedMin = 1;
+
+            int actualMin = ArrayUtils.FindMin(array);
+
+            Assert.AreEqual(expectedMin, actualMin);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMax_EmptyArray_ThrowsException()
+        {
+            int[] array = { };
+            ArrayUtils.FindMax(array);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMin_EmptyArray_ThrowsException()
+        {
+            int[] array = { };
+            ArrayUtils.FindMin(array);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMax_NullArray_ThrowsException()
+        {
+            int[] array = null;
+            ArrayUtils.FindMax(array);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMin_NullArray_ThrowsException()
+        {
+            int[] array = null;
+            ArrayUtils.FindMin(array);
+        }
+    }
+}
---
+++ Lab8_demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab8_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
